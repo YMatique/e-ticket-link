0ec7b7eff020cd0411ef65055c6f6051db86621e
Yuvi Matique <86082620+YMatique@users.noreply.github.com>
2025-10-14 21:07:51 +0200

    feat: Add initial layout for guest application with login form

---
+++ resources/views/layouts/app-guest.blade.php
@@ -0,0 +1,177 @@
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>Limitless - Responsive Web Application Kit by Eugene Kopyov</title>
+
+	<!-- Global stylesheets -->
+	<link href="template/assets/fonts/inter/inter.css" rel="stylesheet" type="text/css">
+	<link href="template/assets/icons/phosphor/styles.min.css" rel="stylesheet" type="text/css">
+	<link href="template/assets/css/ltr/all.min.css" id="stylesheet" rel="stylesheet" type="text/css">
+	<!-- /global stylesheets -->
+
+	<!-- Core JS files -->
+	<script src="template/assets/demo/demo_configurator.js"></script>
+	<script src="template/assets/js/bootstrap/bootstrap.bundle.min.js"></script>
+	<!-- /core JS files -->
+
+	<!-- Theme JS files -->
+	<script src="template/assets/js/app.js"></script>
+	<!-- /theme JS files -->
+
+</head>
+
+<body>
+
+	<!-- Main navbar -->
+	{{-- <div class="navbar navbar-dark navbar-static py-2">
+		<div class="container-fluid">
+			<div class="navbar-brand">
+				<a href="index.html" class="d-inline-flex align-items-center">
+					<img src="template/assets/images/logo_icon.svg" alt="">
+					<img src="template/assets/images/logo_text_light.svg" class="d-none d-sm-inline-block h-16px ms-3" alt="">
+				</a>
+			</div>
+
+			<div class="d-flex justify-content-end align-items-center ms-auto">
+				<ul class="navbar-nav flex-row">
+					<li class="nav-item">
+						<a href="#" class="navbar-nav-link navbar-nav-link-icon rounded ms-1">
+							<div class="d-flex align-items-center mx-md-1">
+							<i class="ph-lifebuoy"></i>
+							<span class="d-none d-md-inline-block ms-2">Support</span>
+						</div>
+						</a>
+					</li>
+					<li class="nav-item">
+						<a href="#" class="navbar-nav-link navbar-nav-link-icon rounded ms-1">
+							<div class="d-flex align-items-center mx-md-1">
+							<i class="ph-user-circle-plus"></i>
+							<span class="d-none d-md-inline-block ms-2">Register</span>
+						</div>
+						</a>
+					</li>
+					<li class="nav-item">
+						<a href="#" class="navbar-nav-link navbar-nav-link-icon rounded ms-1">
+							<div class="d-flex align-items-center mx-md-1">
+							<i class="ph-user-circle"></i>
+							<span class="d-none d-md-inline-block ms-2">Login</span>
+						</div>
+						</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div> --}}
+	<!-- /main navbar -->
+
+
+	<!-- Page content -->
+	<div class="page-content">
+
+		<!-- Main content -->
+		<div class="content-wrapper">
+
+			<!-- Inner content -->
+			<div class="content-inner">
+
+				<!-- Content area -->
+				<div class="content d-flex justify-content-center align-items-center">
+
+					<!-- Login form -->
+					<form class="login-form" action="index.html">
+						<div class="card mb-0">
+							<div class="card-body">
+								<div class="text-center mb-3">
+									<div class="d-inline-flex align-items-center justify-content-center mb-4 mt-2">
+										<img src="template/assets/images/logo_icon.svg" class="h-48px" alt="">
+									</div>
+									<h5 class="mb-0">Login to your account</h5>
+									<span class="d-block text-muted">Enter your credentials below</span>
+								</div>
+
+								<div class="mb-3">
+									<label class="form-label">Username</label>
+									<div class="form-control-feedback form-control-feedback-start">
+										<input type="text" class="form-control" placeholder="john@doe.com">
+										<div class="form-control-feedback-icon">
+											<i class="ph-user-circle text-muted"></i>
+										</div>
+									</div>
+								</div>
+
+								<div class="mb-3">
+									<label class="form-label">Password</label>
+									<div class="form-control-feedback form-control-feedback-start">
+										<input type="password" class="form-control" placeholder="•••••••••••">
+										<div class="form-control-feedback-icon">
+											<i class="ph-lock text-muted"></i>
+										</div>
+									</div>
+								</div>
+
+								<div class="mb-3">
+									<button type="submit" class="btn btn-primary w-100">Sign in</button>
+								</div>
+
+								<div class="text-center">
+									<a href="login_password_recover.html">Forgot password?</a>
+								</div>
+							</div>
+						</div>
+					</form>
+					<!-- /login form -->
+
+				</div>
+				<!-- /content area -->
+
+
+				<!-- Footer -->
+				{{-- <div class="navbar navbar-sm navbar-footer border-top">
+					<div class="container-fluid">
+						<span>&copy; 2022 <a href="https://themeforest.net/item/limitless-responsive-web-application-kit/13080328">Limitless Web App Kit</a></span>
+
+						<ul class="nav">
+							<li class="nav-item">
+								<a href="https://kopyov.ticksy.com/" class="navbar-nav-link navbar-nav-link-icon rounded" target="_blank">
+									<div class="d-flex align-items-center mx-md-1">
+										<i class="ph-lifebuoy"></i>
+										<span class="d-none d-md-inline-block ms-2">Support</span>
+									</div>
+								</a>
+							</li>
+							<li class="nav-item ms-md-1">
+								<a href="https://demo.interface.club/limitless/demo/Documentation/index.html" class="navbar-nav-link navbar-nav-link-icon rounded" target="_blank">
+									<div class="d-flex align-items-center mx-md-1">
+										<i class="ph-file-text"></i>
+										<span class="d-none d-md-inline-block ms-2">Docs</span>
+									</div>
+								</a>
+							</li>
+							<li class="nav-item ms-md-1">
+								<a href="https://themeforest.net/item/limitless-responsive-web-application-kit/13080328?ref=kopyov" class="navbar-nav-link navbar-nav-link-icon text-primary bg-primary bg-opacity-10 fw-semibold rounded" target="_blank">
+									<div class="d-flex align-items-center mx-md-1">
+										<i class="ph-shopping-cart"></i>
+										<span class="d-none d-md-inline-block ms-2">Purchase</span>
+									</div>
+								</a>
+							</li>
+						</ul>
+					</div>
+				</div> --}}
+				<!-- /footer -->
+
+			</div>
+			<!-- /inner content -->
+
+		</div>
+		<!-- /main content -->
+
+	</div>
+	<!-- /page content -->
+
+</body>
+</html>
